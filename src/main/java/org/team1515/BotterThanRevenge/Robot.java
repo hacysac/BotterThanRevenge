283172858bc9db39f19027b250920682124206fa
Hayley <96212232+hacysac@users.noreply.github.com>
2024-01-10 22:26:46 -0800

    removed ctre configs

--- src/main/java/org/team1515/BotterThanRevenge/Robot.java
+++ src/main/java/org/team1515/BotterThanRevenge/Robot.java
@@ -4,8 +4,6 @@
 
 package org.team1515.BotterThanRevenge;
 
-import com.team364.swervelib.util.CTREConfigs;
-
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
