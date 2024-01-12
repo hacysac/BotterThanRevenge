feda8657943b4f7cfa4ee3bb4ce3d1d93aec3df7
Daniel-Marmo <danielmarmo@icloud.com>
2024-01-11 17:10:51 -0800

    Added climber code; fixed error with import

--- src/main/java/org/team1515/BotterThanRevenge/Commands/ClimberDown.java
+++ src/main/java/org/team1515/BotterThanRevenge/Commands/ClimberDown.java
@@ -1,9 +1,24 @@
 package org.team1515.BotterThanRevenge.Commands;
 
-import edu.wpi.first.wpilibj2.command.Command;
-
 import org.team1515.BotterThanRevenge.Subsystems.Climber;
 
+import edu.wpi.first.wpilibj2.command.Command;
+
 public class ClimberDown extends Command {
-    
+    private final Climber climber;
+
+    public ClimberDown(Climber climber) {
+        this.climber = climber;
+        addRequirements(climber);
+    }
+
+    @Override
+    public void execute() {
+        climber.down();
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        climber.end();
+    }
 }
