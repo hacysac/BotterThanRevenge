67264944928b8b95893616de3a8b0c1fc3630c96
elle1300 <97467814+elle1300@users.noreply.github.com>
2024-01-14 13:49:30 -0800

    work in progress added a second motor for the intake

--- src/main/java/org/team1515/BotterThanRevenge/Subsystems/Intake.java
+++ src/main/java/org/team1515/BotterThanRevenge/Subsystems/Intake.java
@@ -9,23 +9,24 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Intake extends SubsystemBase {
     
-    private CANSparkMax intake;
+    private CANSparkMax bottomIntake;
+    private CANSparkMax topIntake;
 
     public Intake(){
-                intake = new CANSparkMax(RobotMap.INTAKE_ID, MotorType.kBrushless);
+        bottomIntake = new CANSparkMax(RobotMap.INTAKE_ID, MotorType.kBrushless);
     }
 
     public void in(){
-        intake.set(RobotMap.INTAKE_SPEED);
+        bottomIntake.set(RobotMap.INTAKE_SPEED);
 
     }
 
     public void out(){
-                intake.set(-RobotMap.INTAKE_SPEED);
+        bottomIntake.set(-RobotMap.INTAKE_SPEED);
 
     }
 
     public void end(){
-        intake.set(0);
+        bottomIntake.set(0);
     }
 }
