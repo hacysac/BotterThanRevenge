cc4654af07dce1d6468e4700ae361e06c39796d5
imagodsobeit <imagodsobeit@gmail.com>
2024-01-14 11:23:38 -0800

    resolve merge conflicts in climber

--- src/main/java/org/team1515/BotterThanRevenge/Commands/ClimberDown.java
+++ src/main/java/org/team1515/BotterThanRevenge/Commands/ClimberDown.java
@@ -4,6 +4,24 @@ import edu.wpi.first.wpilibj2.command.Command;
 
 import org.team1515.BotterThanRevenge.Subsystems.Climber;
 
+import edu.wpi.first.wpilibj2.command.Command;
+
 public class ClimberDown extends Command {
-    
+
+    private final Climber climber;
+
+    public ClimberDown(Climber climber) {
+        this.climber = climber;
+        addRequirements(climber);
+    }
+
+    @Override
+    public void execute() {
+        climber.down();
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        climber.end();
+    }
 }
--- src/main/java/org/team1515/BotterThanRevenge/Subsystems/Climber.java
+++ src/main/java/org/team1515/BotterThanRevenge/Subsystems/Climber.java
@@ -1,23 +1,36 @@
 package org.team1515.BotterThanRevenge.Subsystems;
 
+import org.team1515.BotterThanRevenge.RobotMap;
+
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Climber extends SubsystemBase {
-    
-    private CANSparkMax lClimber;
     private CANSparkMax rClimber;
+    private CANSparkMax lClimber;
 
     public Climber(){
 
+        lClimber = new CANSparkMax(RobotMap.L_CLIMBER_ID, MotorType.kBrushless);
+        rClimber = new CANSparkMax(RobotMap.R_CLIMBER_ID, MotorType.kBrushless);
     } 
 
     public void up(){
 
+        lClimber.set(RobotMap.CLIMBER_SPEED);
+        rClimber.set(RobotMap.CLIMBER_SPEED);
     }
 
     public void down(){
-        
+
+        lClimber.set(-RobotMap.CLIMBER_SPEED);
+        rClimber.set(-RobotMap.CLIMBER_SPEED);
     }
+
+    public void end() {
+        lClimber.set(0);
+        rClimber.set(0);
+    } 
 }
