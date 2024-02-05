4ee9a0e9154506bed9b5cdc29f74d96202aee929
Hayley <hayley.chechel@gmail.com>
2024-02-05 09:38:14 -0800

    added time control to AutoIntake

--- src/main/java/org/team1515/BotterThanRevenge/Commands/AutoIntakeIn.java
+++ src/main/java/org/team1515/BotterThanRevenge/Commands/AutoIntakeIn.java
@@ -8,12 +8,28 @@ import org.team1515.BotterThanRevenge.Subsystems.Intake;
 public class AutoIntakeIn extends Command {
     private final Intake intake; 
     private final Indexer indexer;
+    private double time;
+    private double startTime;
 
     public AutoIntakeIn(Intake intake, Indexer indexer) {
         this.intake = intake;
         this.indexer = indexer;
+        this.time = 1000; // 1000 seconds will not be reached
         addRequirements(intake);
         addRequirements(indexer);
+    }
+
+    public AutoIntakeIn(Intake intake, Indexer indexer, double time) {
+        this.intake = intake;
+        this.indexer = indexer;
+        this.time = time;
+        addRequirements(intake);
+        addRequirements(indexer);
+    }
+
+    @Override
+    public void initialize() {
+        this.startTime = System.currentTimeMillis();
     }
 
     @Override
@@ -24,7 +40,7 @@ public class AutoIntakeIn extends Command {
 
     @Override
     public boolean isFinished() {
-        return indexer.isBlocked() || intake.getDone();
+        return indexer.isBlocked() || intake.getDone() || (System.currentTimeMillis()-startTime) >= time*1000;
     }
 
     @Override
