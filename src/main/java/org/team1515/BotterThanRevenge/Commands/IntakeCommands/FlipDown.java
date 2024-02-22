2f3cf322b0c38aee2eee5ccefc8cbb11d13d4910
Hayley <hayley.chechel@gmail.com>
2024-02-22 09:52:17 -0800

    uncommented auto, removed unnessicary imports

--- src/main/java/org/team1515/BotterThanRevenge/Commands/IntakeCommands/FlipDown.java
+++ src/main/java/org/team1515/BotterThanRevenge/Commands/IntakeCommands/FlipDown.java
@@ -3,7 +3,6 @@ package org.team1515.BotterThanRevenge.Commands.IntakeCommands;
 import edu.wpi.first.wpilibj2.command.Command;
 
 import org.team1515.BotterThanRevenge.Subsystems.Flip;
-import org.team1515.BotterThanRevenge.Subsystems.Intake;
 
 public class FlipDown extends Command {
     private final Flip flip; 
