e41ba22388cc40fc12b44228fe6f88f05c4464a2
Hayley <hayley.chechel@gmail.com>
2024-02-25 15:13:09 -0800

    fixed auto measurements

--- src/main/java/org/team1515/BotterThanRevenge/RobotMap.java
+++ src/main/java/org/team1515/BotterThanRevenge/RobotMap.java
@@ -16,10 +16,10 @@ public class RobotMap {
 
     public static final double SUBWOOFER_DEPTH = 37;
     public static final double SUBWOOFER_LONG_WIDTH = 80;
-    public static final double SUBWOOFER_TO_NOTE = 68.5;
-    public static final double SUBWOOFER_TO_AMP = 63.5;
+    public static final double SUBWOOFER_TO_NOTE = 78;
+    public static final double SUBWOOFER_TO_AMP = 64;
     public static final double NOTE_TO_NOTE = 57;
-    public static final double WALL_TO_AMP = 76.1;
+    public static final double WALL_TO_AMP = 73;
     public static final double WALL_TO_CENTER = 325.5;
     public static final double SUBWOOFER_TO_CENTER = (WALL_TO_CENTER-SUBWOOFER_DEPTH) - 7; // -7 for note TODO check
     public static final double AMP_TO_CENTER = (WALL_TO_CENTER-WALL_TO_AMP) - 7; // -7 for note TODO check
@@ -42,38 +42,33 @@ public class RobotMap {
     // Shooter
     public static final int L_SHOOTER_ID = 20;
     public static final int R_SHOOTER_ID = 21;
-    public static final double SPEAKER_SPEED = 0.8; // TODO
-    public static final double AMP_SPEED = 0.15; // TODO
+    public static final double SPEAKER_SPEED = 0.7; // TODO
+    public static final double AMP_SPEED = 0.175; // TODO
 
     // Indexer
     public static final int INDEXER_ID = 22;
-    public static final int INDEX_SENSOR_CHANNEL = 2; 
-    public static final double INDEXER_SPEED = 0.5;
+    public static final int INDEX_SENSOR_CHANNEL = 2; // TODO
+    public static final double INDEXER_SPEED = 0.5;// TODO 
 
     //Intake
     public static final int TOP_INTAKE_ID = 23;
     public static final int BOTTOM_INTAKE_ID = 24;
-    public static final double LOWER_INTAKE_SPEED = 0.4; 
-    public static final double UPPER_INTAKE_SPEED = 0.5; 
+    public static final double INTAKE_SPEED = 0.5; //TODO
 
     public static final int FLIP_INTAKE_ID = 25;
     public static final int FLIP_UPPER_SENSOR_CHANNEL = -1; //TODO
     public static final int FLIP_LOWER_SENSOR_CHANNEL = -1; // TODO
-    public static final int FLIP_CANCODER_ID = 30; 
-    public static final double FLIP_UP_SPEED = 0.15; //TODO
-    public static final double FLIP_DOWN_SPEED = 0.05; //TODO should be less than ff value
-    public static final double FLIP_DOWN_VALUE = 0.43; //TODO
-    public static final double FLIP_UP_VALUE = 0.93; //TODO
-    public static final double FLIP_TOP_VALUE = 0.85; //TODO
-    public static final double FLIP_MID_VALUE = 0.77; //TODO
+    public static final int FLIP_CANCODER_ID = 30; //TODO
+    public static final double FLIP_UP_SPEED = 0.2; //TODO
+    public static final double FLIP_DOWN_SPEED = 0.04; //TODO should be less than ff value
+    public static final double FLIP_DOWN_VALUE = -1; //TODO
+    public static final double FLIP_UP_VALUE = -1; //TODO
 
     //Climber
-    public static final int L_CLIMBER_ID = 27;
-    public static final int R_CLIMBER_ID = 26;
-    public static final int L_CLIMBER_SENSOR_CHANNEL = 0; 
-    public static final int R_CLIMBER_SENSOR_CHANNEL = 1; 
-    public static final double CLIMBER_UP_SPEED = 0.35; // TODO
-    public static final double CLIMBER_DOWN_SPEED = 0.5; // TODO
+    public static final int L_CLIMBER_ID = 26;
+    public static final int R_CLIMBER_ID = 27;
+    public static final int L_CLIMBER_SENSOR_CHANNEL = 0; //TODO
+    public static final int R_CLIMBER_SENSOR_CHANNEL = 1; //TODO
+    public static final double CLIMBER_SPEED = 0.25; // TODO
     public static final double CLIMBER_EXTENTION_LIMIT = 23.5; //TODO
-    public static final int CLIMBER_CURRENT_LIMIT = 30; 
 }
