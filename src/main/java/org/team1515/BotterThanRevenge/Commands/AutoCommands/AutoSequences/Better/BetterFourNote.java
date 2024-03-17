4097756dfc7684da80f45f6e8363ccc4aa4c84c0
Hayley <hayley.chechel@gmail.com>
2024-03-17 08:23:04 -0700

    OFFSETS

--- src/main/java/org/team1515/BotterThanRevenge/Commands/AutoCommands/AutoSequences/Better/BetterFourNote.java
+++ src/main/java/org/team1515/BotterThanRevenge/Commands/AutoCommands/AutoSequences/Better/BetterFourNote.java
@@ -2,10 +2,12 @@ package org.team1515.BotterThanRevenge.Commands.AutoCommands.AutoSequences.Bette
 
 import org.team1515.BotterThanRevenge.RobotMap;
 import org.team1515.BotterThanRevenge.Commands.AutoCommands.driveLine;
+import org.team1515.BotterThanRevenge.Commands.ClimberCommands.ZeroClimber;
 import org.team1515.BotterThanRevenge.Commands.IndexerCommands.AutoFeed;
 import org.team1515.BotterThanRevenge.Commands.IntakeCommands.AutoIntakeIn;
 import org.team1515.BotterThanRevenge.Commands.IntakeCommands.FlipDown;
 import org.team1515.BotterThanRevenge.Commands.IntakeCommands.FlipUp;
+import org.team1515.BotterThanRevenge.Subsystems.Climber;
 import org.team1515.BotterThanRevenge.Subsystems.Drivetrain;
 import org.team1515.BotterThanRevenge.Subsystems.Flip;
 import org.team1515.BotterThanRevenge.Subsystems.Indexer;
@@ -19,9 +21,9 @@ import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 public class BetterFourNote extends SequentialCommandGroup{
-    public BetterFourNote(Drivetrain drivetrain, Shooter shooter, Indexer indexer, Intake intake, Flip flip, double direction){        
-        double subwooferToNoteX = Units.inchesToMeters(RobotMap.SUBWOOFER_TO_NOTE - RobotMap.CHASSIS_WIDTH + 1);
-        double subwooferToNoteY = -direction * Units.inchesToMeters(RobotMap.NOTE_TO_NOTE - (0.5 * RobotMap.CHASSIS_WIDTH) + 3);
+    public BetterFourNote(Drivetrain drivetrain, Shooter shooter, Indexer indexer, Intake intake, Flip flip, Climber climber, double direction){        
+        double subwooferToNoteX = Units.inchesToMeters(RobotMap.SUBWOOFER_TO_NOTE - RobotMap.CHASSIS_WIDTH + 4);
+        double subwooferToNoteY = -direction * Units.inchesToMeters(RobotMap.NOTE_TO_NOTE - (0.5 * RobotMap.CHASSIS_WIDTH) + 4);
 
         Point pose0 = new Point(0, 0);
         Point pose1 = new Point(subwooferToNoteX, 0);
@@ -74,5 +76,6 @@ public class BetterFourNote extends SequentialCommandGroup{
         //Drive Back
         addCommands(new InstantCommand(()->shooter.end()));
         addCommands(new driveLine(drivetrain, 0, finalPose, 1).withTimeout(2));
+        addCommands(new ZeroClimber(climber));
     }
 }
