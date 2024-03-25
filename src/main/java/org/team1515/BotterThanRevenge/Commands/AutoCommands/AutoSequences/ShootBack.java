3cb65e05b4606212c11eedea7c76c02e47b4a483
Hayley <96212232+hacysac@users.noreply.github.com>
2024-03-25 16:21:40 -0700

    Added shoot to drive back

--- src/main/java/org/team1515/BotterThanRevenge/Commands/AutoCommands/AutoSequences/ShootBack.java
+++ src/main/java/org/team1515/BotterThanRevenge/Commands/AutoCommands/AutoSequences/ShootBack.java
@@ -4,7 +4,7 @@ import org.team1515.BotterThanRevenge.RobotMap;
 import org.team1515.BotterThanRevenge.Commands.AutoCommands.driveLine;
 import org.team1515.BotterThanRevenge.Commands.IndexerCommands.AutoFeed;
 import org.team1515.BotterThanRevenge.Commands.IntakeCommands.FlipUp;
-import org.team1515.BotterThanRevenge.Subsystems.Drivetrain;
+import org.team1515.BotterThanRevenge.Subsystems.Drivetrain;`
 import org.team1515.BotterThanRevenge.Subsystems.Flip;
 import org.team1515.BotterThanRevenge.Subsystems.Indexer;
 import org.team1515.BotterThanRevenge.Subsystems.Shooter;
@@ -15,20 +15,18 @@ import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
-public class ShootBack extends SequentialCommandGroup {
-    public ShootBack(Drivetrain drivetrain, Shooter shooter, Indexer indexer, Flip flip, int direction){
-        Point finalPose = new Point(Units.inchesToMeters(RobotMap.ROBOT_STARTING_ZONE_WIDTH + 5), direction*Units.inchesToMeters(63));
-        
-        addCommands(new InstantCommand(()->shooter.shootSpeaker()));
-        addCommands(new FlipUp(flip));
-        addCommands(Commands.waitSeconds(0.5));
-        
-        //Score Stoed Note
-        addCommands(new AutoFeed(indexer, RobotMap.AUTO_FEED_TIME).withTimeout(2));
-        addCommands(Commands.waitSeconds(0.5));
-        addCommands(new InstantCommand(()->shooter.end()));
-
-        //Drive Away
-        addCommands(new driveLine(drivetrain, 0, finalPose, 1).withTimeout(2));
+public class DriveBack2Seq extends SequentialCommandGroup {
+    public DriveBack2Seq(Shooter shooter, Flip flip, Indexer indexer, Drivetrain drivetrain){
+        addCommands(Commands.parallel(
+           new FlipUp(flip),
+           new InstantCommand(()->shooter.shootSpeaker())
+       ).withTimeout(2));
+       addCommands(Commands.waitSeconds(1));
+       addCommands(new AutoFeed(indexer, RobotMap.AUTO_FEED_TIME).withTimeout(2));
+       addCommands(Commands.waitSeconds(1));
+       addCommands(new InstantCommand(()->shooter.end()));
+        Point finalPose = new Point(Units.inchesToMeters(RobotMap.ROBOT_STARTING_ZONE_WIDTH + 5), 0);
+        //addCommands(new FlipUp(flip).withTimeout(2));
+        addCommands(new driveLine(drivetrain, 0, finalPose, 3).withTimeout(2));
     }
 }
