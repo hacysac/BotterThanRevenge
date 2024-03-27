1fb3c5db7bfc7fdb90605fcab5d2c11749fe4c65
Daniel Salkin <96212232+hacysac@users.noreply.github.com>
2024-03-26 17:16:56 -0700

    Shooter PID

--- src/main/java/org/team1515/BotterThanRevenge/Subsystems/Shooter.java
+++ src/main/java/org/team1515/BotterThanRevenge/Subsystems/Shooter.java
@@ -4,6 +4,8 @@ import org.team1515.BotterThanRevenge.RobotMap;
 
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkPIDController;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -18,16 +20,57 @@ public class Shooter extends SubsystemBase {
 
     double leftMax;
     double rightMax;
+    private RelativeEncoder lEncoder;
+    private SparkPIDController lPidController;
+
+    public double kP,kI,kD,kIz,kFF,kMaxOutput,kMinOutput,maxRPM,maxVel,minVel,maxAcc,allowedErr;
     
     public Shooter(){
         lShooter = new CANSparkMax(RobotMap.L_SHOOTER_ID, MotorType.kBrushless);
         rShooter = new CANSparkMax(RobotMap.R_SHOOTER_ID, MotorType.kBrushless);
+
+        lPidController = lShooter.getPIDController();
+        lEncoder = lShooter.getEncoder();
+
         rShooter.follow(lShooter, true);
 
         lShooter.setSmartCurrentLimit(RobotMap.SHOOTER_CURRENT_LIMIT);
         rShooter.setSmartCurrentLimit(RobotMap.SHOOTER_CURRENT_LIMIT);
         lShooter.burnFlash();
         rShooter.burnFlash();
+        //PID coeficients
+        kP = 5e-5;
+        kI = 1e-6;
+        kD = 0;
+        kIz = 0;
+        kFF = 0.000156;
+        kMaxOutput = 1;
+        kMinOutput = -1;
+        maxRPM = 5700;
+        //SmartMotion coeficients
+        maxVel = 5500;
+        maxAcc = 5000;
+        //set PID coefficients
+        lPidController.setP(kP);
+        lPidController.setI(kI);
+        lPidController.setD(kD);
+        lPidController.setIZone(kIz);
+        lPidController.setFF(kFF);
+        lPidController.setOutputRange(kMinOutput, kMaxOutput);
+        //set smartmotion Constants
+        int smartMotionSlot = 0;
+        lPidController.setSmartMotionMaxVelocity(maxVel, smartMotionSlot);
+        lPidController.setSmartMotionMinOutputVelocity(minVel, smartMotionSlot);
+        lPidController.setSmartMotionMaxAccel(maxAcc, smartMotionSlot);
+        lPidController.setSmartMotionMaxAccel(allowedErr, smartMotionSlot);
+
+        SmartDashboard.putNumber("kP", kP);
+        SmartDashboard.putNumber("kI", kI);
+        SmartDashboard.putNumber("kD", kD);
+        SmartDashboard.putNumber("kIz", kIz);
+        SmartDashboard.putNumber("kFF", kFF);
+        SmartDashboard.putNumber("kMax", kMaxOutput);
+        SmartDashboard.putNumber("kMin", kMinOutput);
 
         speaker = false;
         amp = false;
