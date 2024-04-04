18a729e4e720a76439b081aa150e2856dc28ce5b
Daniel Salkin <71471128+DanielSalkin@users.noreply.github.com>
2024-04-03 18:48:35 -0700

    hayley simple autoselector error

--- src/main/java/org/team1515/BotterThanRevenge/RobotContainer.java
+++ src/main/java/org/team1515/BotterThanRevenge/RobotContainer.java
@@ -172,25 +172,27 @@ public class RobotContainer {
     //     return new Stay(shooter, flip, indexer);
     // }
     int selected = 0;
-    if(controller1){selected = 0;}
-    if(controller2){selected = 1;}
-    if(controller3){selected = 2;}
-    if(controller4){selected = 3;}
-    if(controller5){selected = 4;}
-    if(controller6){selected = 5;}
+    if(controller1){selected = 1;}
+    if(controller2){selected = 2;}
+    if(controller3){selected = 3;}
+    if(controller4){selected = 4;}
+    if(controller5){selected = 5;}
+    if(controller6){selected = 6;}
 
     switch (selected) {
-      case 0:
-        return new DriveBackSeq(drivetrain, flip);
       case 1:
-        return new FourNoteSeq(drivetrain, shooter, indexer, intake, flip, team);
+        return new DriveBackSeq(drivetrain, flip);
       case 2:
-        return new FourNoteSeq(drivetrain, shooter, indexer, intake, flip, -team);
+        return new FourNoteSeq(drivetrain, shooter, indexer, intake, flip, team);
       case 3:
-        return new FiveNoteSeq(drivetrain, shooter, indexer, intake, flip, -team);
+        return new FourNoteSeq(drivetrain, shooter, indexer, intake, flip, -team);
       case 4:
-        return new ShootBack(drivetrain, shooter,flip, indexer);
+        return new FiveNoteSeq(drivetrain, shooter, indexer, intake, flip, -team);
       case 5:
+        return new ShootBack(drivetrain, shooter,flip, indexer);
+      case 6:
+        return new Stay(shooter, flip, indexer);
+      default:
         return new Stay(shooter, flip, indexer);
     }
   }
